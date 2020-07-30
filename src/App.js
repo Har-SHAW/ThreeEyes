2f5922d63f1cacea964ca61d04eba4cf3067fc84
Har-SHAW <8888harsha4444@gmail.com>
2020-07-30 16:58:44 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/MainComponent";
-import Search from './components/searchdrop'
 class App extends Component {
   render() {
     return (
       <div>
         <Main />
-        <Search/>
       </div>
     );
   }
